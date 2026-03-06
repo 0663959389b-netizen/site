00ea28f67f55a4a5fc0fde2a217de48801888a2d
Poveda Bohdana <0663959389@gmail.com>
2026-03-06 14:54:53 +0200

    lab3

--- DjangoProject2/urls.py
+++ DjangoProject2/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from shop import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'), # Головна сторінка
-    path('menu/', views.menu_view, name='menu'), # Сторінка з меню
+    path('', include('shop.urls')),
 ]
